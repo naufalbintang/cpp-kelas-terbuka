cb81dfa2b03e3a7c19171d6cdc7c29b29238cd3f
naufalbintang <naufalbintangpradana@gmail.com>
2024-09-15 07:31:24 +0700

    03 - Variabel

---
+++ 03 - Variabel/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    a = 10;
+    cout << a << endl;
+
+    int b;
+    cout << "Masukkan nilai: ";
+    cin >> b;
+    cout << "Nilai yang anda masukkan adalah: ";
+    cout << b;
+
+    cin.get();
+    return 0;
+}
