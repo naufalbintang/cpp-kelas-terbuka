5d2de232818657182115888da457247a1923bb03
naufalbintang <naufalbintangpradana@gmail.com>
2024-09-15 07:09:56 +0700

    02 - Printing

---
+++ 02 - Printing/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main(){
+    std::cout << "halo sodara-sodara sekalian \n";
+    std::cout << "Ini adalah baris baru.";
+    std::cout << " Ini bukan baris baru.";
+    std::cout << " ini akhir kalimat." << std::endl;
+    std::cin.get();
+    return 0;
+}
