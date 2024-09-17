858be20cd88dfb5b1c034f0e8ae935c72c7a3353
unknown <naufalbintangpradana@gmail.com>
2024-09-17 14:25:05 +0700

    08 - If Statement

---
+++ 08 - If Statement/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+
+    cout << "Masukkan angka: ";
+    cin >> a;
+
+    // if statement
+    // kondisi dalam bentuk boolean
+    if (a < 3){
+        cout << "halooo" << endl;
+    }
+
+    cout << "program selesai" << endl;
+
+    cin.get();
+    return 0;
+}
