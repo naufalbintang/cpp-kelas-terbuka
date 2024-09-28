62d5f19de97d01bfeed2a24d8a1b2da115429588
unknown <naufalbintangpradana@gmail.com>
2024-09-28 23:29:31 +0700

    update tes.cpp

--- tes.cpp
+++ tes.cpp
@@ -1,76 +1,16 @@
 #include <iostream>
+// #include <string>
 using namespace std;
 
-char input_operasi(){
-    char operasi;
-    cout << "Operasi apa yang akan anda lakukan (+, -, /, *)? ";
-    cin >> operasi;
-    return operasi;
-}
-
-float input_angka(){
-    float angka;
-    cout << "Masukkan angka pertama: ";
-    cin >> angka;
-    return angka;
-}
-
-float fungsi_penjumlahan(float angka1, float angka2){
-    float hasil = angka1 + angka2; 
-    return hasil;
-}
-float fungsi_pengurangan(float angka1, float angka2){
-    float hasil = angka1 - angka2; 
-    return hasil;
-}
-float fungsi_perkalian(float angka1, float angka2){
-    float hasil = angka1 * angka2; 
-    return hasil;
-}
-float fungsi_pembagian(float angka1, float angka2){
-    float hasil = angka1 / angka2; 
-    return hasil;
-}
-
-void display_hasil(float angka1, char operasi, float angka2, float hasil){
-    cout << angka1 << " " << operasi << " " << angka2 << " = " << hasil;  
-}
-
-
 int main(){
-    float angka1, angka2, hasil;
-    char operasi;
-    cout << "Selamat Datang di Program Kalkulator Sederhana." << endl << endl;
 
-    // input user operasi
-    operasi = input_operasi();
+    string greeting = "hello world";
+    int greeting_len = greeting.length();
 
-    // input user angka1
-    angka1 = input_angka();
-
-    // input user angka2
-    angka2 = input_angka();
-    
-    switch (operasi){
-        case '+':
-            hasil = fungsi_penjumlahan(angka1, angka2);
-            display_hasil(angka1, operasi, angka2, hasil);
-            break;
-        case '-':
-            hasil = fungsi_pengurangan(angka1, angka2);
-            display_hasil(angka1, operasi, angka2, hasil);
-            break;
-        case '*':
-            hasil = fungsi_perkalian(angka1, angka2);
-            display_hasil(angka1, operasi, angka2, hasil);
-            break;
-        case '/':
-            hasil = fungsi_pembagian(angka1, angka2);
-            display_hasil(angka1, operasi, angka2, hasil);
-            break;
-        default:
-            cout << "Operasi tidak valid." << endl;
+    cout << endl << greeting << endl;
+    for (int i = 1; i <= greeting_len; i++){
+        cout << "=";
     }
-    
+
     return 0;
 }
