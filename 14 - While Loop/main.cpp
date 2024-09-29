ea3aa825ab055fa320269717da1aad29e31b73e6
naufalbintang <naufalbintangpradana@gmail.com>
2024-09-29 08:27:32 +0700

    14 - While Loop

---
+++ 14 - While Loop/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    while (a <= 10){
+        cout << "hore " << a << endl;
+        a += 1;
+    }
+
+    cout << "selesai" << endl;
+
+    cin.get();
+    return 0;
+}
