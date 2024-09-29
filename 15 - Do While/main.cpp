ec3b5907f0719e60d478a1bcc25f061268a4dde6
naufalbintang <naufalbintangpradana@gmail.com>
2024-09-29 08:37:34 +0700

    15 - Do While

---
+++ 15 - Do While/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 1;
+    do{
+        cout << "hore " << a << endl;
+        a++;
+    }while(a < 10);
+    
+
+    cin.get();
+    return 0;
+}
